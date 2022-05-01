af91dfd8f7d5cf70fdfa8649458a192882c5497c
endurovojta173 <vojtechbrenek173@gmail.com>
2022-05-01 15:27:07 +0200

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Vlastni
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Pivo pivo = new Pivo(30, "Plzeň", 0.5);
+        Rum rum = new Rum(75, "božkov", 0.2, false);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rum.Nalit(1);
+            rum.SetKelimekZaRum(true);
+            // nastaví kelímek na true, protože si kupuje rum
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pivo.Nalit(1); // nalije pivo
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           MessageBox.Show("Kolik zaplatit za pivo: " + pivo.KolikZaplatit() + "\nKolik zaplatit za rum: "+rum.KolikZaplatit()); //kolik platím za pivo a rum
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pivo.Zaplatit(50); //platím za pivo
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            rum.Zaplatit(75); //platím za rum
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (!rum.GetKelimekZaRum()) //výpis podle kelímku na rum
+            {
+                MessageBox.Show(pivo.ToString() + "\n\n" + rum.ToString() + "\nNemáte kelímek na rum");
+            }
+            else
+            {
+                MessageBox.Show(pivo.ToString() + "\n\n" + rum.ToString() + "\nMáte kelímek na rum");
+            }
+        }
+    }
+}
---
+++ Rum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Vlastni
+{
+    class Rum : Pivo
+    {
+        private bool kelimekNaRum = false;
+        public Rum(int cena, string znacka, double objem,bool kelimekZaRum): base(cena,znacka,objem)
+        {
+            this.kelimekNaRum = kelimekZaRum;
+        }
+        public bool GetKelimekZaRum()
+        {
+            return kelimekNaRum;
+        }
+        public void SetKelimekZaRum(bool kelimekNaRum)
+        {
+            this.kelimekNaRum = kelimekNaRum;
+        }
+    }
+}
